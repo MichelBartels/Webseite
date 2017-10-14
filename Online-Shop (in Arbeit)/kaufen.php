f93e351df8bc168dfb6fff5c943d892352342ae8
LabraxAI <michel-bartels@web.de>
2017-10-14 14:05:03 +0200

    Alles entfernt

--- Online-Shop (in Arbeit)/kaufen.php
+++
@@ -1,7 +0,0 @@
-<?php
-    session_start();
-    $id = $_GET["id"];
-    $_SESSION["Warenkorb"] = $_SESSION["Warenkorb"] ?: array();
-    $_SESSION["Warenkorb"][$id] = $_SESSION["Warenkorb"][$id] ? $_SESSION["Warenkorb"][$id] + 1: 1;
-    header("Location: produkt.php?id=" . $id);
-?>
--- Online-Shop (in Arbeit)/warenkorb_leeren.php
+++
@@ -1,5 +0,0 @@
-<?php
-    session_start();
-    unset($_SESSION["Warenkorb"][$_GET["id"]]);
-    header("Location: " . $_SERVER["HTTP_HOST"] . $_GET["url"]);
-?>
