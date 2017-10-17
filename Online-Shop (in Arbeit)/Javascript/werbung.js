74e952f90e241d00402ce35c6cd73d7dc6aeeb9d
LabraxAI <michel-bartels@web.de>
2017-10-17 11:04:30 +0200

    Improved werbung.js

--- Online-Shop (in Arbeit)/Javascript/werbung.js
+++ Online-Shop (in Arbeit)/Javascript/werbung.js
@@ -1,6 +1,13 @@
 function werbung_laden() {
-    let werbung = document.getElementsByTagName("werbung")
-    for (var i = 0; i < werbung.length; i++) {
-        console.log(werbung[i].childNodes)
+    let werbung_tags = document.getElementsByTagName("werbung")
+    let werbung = []
+    for (var i = 0; i < werbung_tags.length; i++) {
+        werbung_nodes = werbung_tags[i].children
+        werbung.push({bilder: [], })
+        for (var i2 = 0; i2 < werbung_nodes.length; i2++) {
+            werbung_seite_nodes = werbung_nodes[i2].children
+            werbung[i].bilder.push(werbung_seite_nodes[0].src)
+        }
     }
+    console.log(werbung)
 }
