ec6bcb9bbc878f125e636ee4e39271868bafb510
oekoklaus <32864685+oekoklaus@users.noreply.github.com>
2017-10-18 11:40:29 +0200

    Create Test.php

---
+++ Test.php
@@ -0,0 +1,29 @@
+<html>
+<head>
+</head>
+<body>
+<h1 id="Beispiel">Beispiel 4: Navigationsleiste mit Überschriften</h1>
+<nav>
+	<ul>
+		<li>
+			<h2>Rubrik 1</h2>
+		</li>
+		<li><a href="#">Beispiel 1</a>
+		</li>
+		<li><a href="#">Beispiel 2</a>
+		</li>
+		<li><a href="#">Beispiel 3</a>
+		</li>
+		<li>
+			<h2>Rubrik 2</h2>
+		</li>
+		<li><span>aktuelle Seite</span>
+		</li>
+		<li><a href="#">Beispiel 5</a>
+		</li>
+		<li><a href="#">Beispiel 6</a>
+		</li>
+	</ul>
+</nav>
+</body>
+</html>
