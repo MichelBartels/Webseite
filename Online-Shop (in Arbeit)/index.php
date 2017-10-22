202cb86c6b2c577d1dbba8b974b54673b8c576f4
LabraxAI <michel-bartels@web.de>
2017-10-22 14:57:23 +0200

    Update index.php

--- Online-Shop (in Arbeit)/index.php
+++ Online-Shop (in Arbeit)/index.php
@@ -1,1 +1,11 @@
-
+<!doctype html>
+<html>
+    <head>
+        <link rel="stylesheet" href="CSS/index.css">
+    </head>
+    <body>
+        <div id="suche">
+            <input type="text">
+        </div>
+    </body>
+</html>
