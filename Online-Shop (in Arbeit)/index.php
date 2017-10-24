1209ec671a71a2c8333915e108a1b861922566aa
LabraxAI <michel-bartels@web.de>
2017-10-24 13:19:16 +0200

    Update index.php

--- Online-Shop (in Arbeit)/index.php
+++ Online-Shop (in Arbeit)/index.php
@@ -8,9 +8,8 @@
             <input type="text" placeholder="Suchen">
         </div>
         <div id="werbung_die_nicht_von_adblockern_geblockt_werden_soll">
-            <div class="werbeseite">
+            <div class="werbeseite" id="werbeseite_1">
                 <h1 id="begruessung">Sie befinden sich auf der Webseite von B&amp;B, ihrem Partner für den Wohnungsbau</h1>
-                <img src="Bilder/Design/Werbung.jpg" class="hintergrund">
             </div>
             <div id="weiter_rechts" class="weiter_button">
             </div>
