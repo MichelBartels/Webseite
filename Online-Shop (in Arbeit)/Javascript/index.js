097ea8e2c1134eac058e1c9936718d276e3835a9
LabraxAI <michel-bartels@web.de>
2017-10-25 16:12:04 +0200

    Update index.js

--- Online-Shop (in Arbeit)/Javascript/index.js
+++ Online-Shop (in Arbeit)/Javascript/index.js
@@ -1,35 +1,25 @@
-window.onload = function() {
-    let direktsuche = document.getElementById("direktsuche")
-    let nicht_direktsuche = document.getElementById("nichtdirektsuche")
-    let produkte = document.getElementById("produkte")
-    direktsuche.addEventListener("input", function() {
-        let suchergebnisse = suchen(direktsuche.value)
-        let neues_html = ""
-        suchergebnisse.forEach(function(element) {
-            neues_html += '<a class="produkt_link" href="produkt.php?id=' + element.ID + '"><div class="produkt">' + element.Name + ": " + element.Beschreibung + "</div></a>"
-        })
-        produkte.innerHTML = neues_html
-    })
-    function hintergrund_loeschen() {
-        document.getElementById("hintergrund").style["opacity"] = "0";
-        document.getElementsByTagName("werbung")[0].style["opacity"] = "0"
-        document.getElementsByTagName("h1")[0].style["opacity"] = "0"
-        direktsuche.style["margin-top"] = "-45vh"
-        direktsuche.style["width"] = "80vw"
-        produkte.style["z-index"] = 10
+let weiter_rechts = document.getElementById("weiter_rechts")
+let weiter_links = document.getElementById("weiter_links")
+let erste_seite = document.getElementById("werbeseite_1")
+let zweite_seite = document.getElementById("werbeseite_2")
+erste_seite.style["transition"] = "opacity 200ms"
+zweite_seite.style["transition"] = "opacity 200ms"
+let erste_seite_sichtbar = true
+weiter_rechts.addEventListener("click", function() {
+    if (erste_seite_sichtbar) {
+        erste_seite.style["opacity"] = 0
+        zweite_seite.style["opacity"] = 1
+    } else {
+        erste_seite.style["opacity"] = 1
+        zweite_seite.style["opacity"] = 0
     }
-    function direktsuche_focus() {
-        direktsuche.id = "direktsuche_focus"
-        nicht_direktsuche.style["opacity"] = "0";
-        hintergrund_loeschen()
+})
+weiter_links.addEventListener("click", function() {
+    if (erste_seite_sichtbar) {
+        erste_seite.style["opacity"] = 0
+        zweite_seite.style["opacity"] = 1
+    } else {
+        erste_seite.style["opacity"] = 1
+        zweite_seite.style["opacity"] = 0
     }
-    direktsuche.addEventListener("focus", direktsuche_focus)
-    function nicht_direktsuche_focus() {
-        nicht_direktsuche.style["margin-top"] = "-70vh"
-        direktsuche.style["opacity"] = "0";
-        hintergrund_loeschen()
-    }
-    for (i = 0; i < nicht_direktsuche.children.length; i++) {
-        nicht_direktsuche.children[i].addEventListener("focus", nicht_direktsuche_focus)
-    }
-}
+})
