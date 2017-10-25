ca24db2750714a71dcba139967dbd91c5a58cfb5
LabraxAI <michel-bartels@web.de>
2017-10-25 17:40:44 +0200

    Update index.js

--- Online-Shop (in Arbeit)/Javascript/index.js
+++ Online-Shop (in Arbeit)/Javascript/index.js
@@ -5,6 +5,8 @@ let zweite_seite = document.getElementById("werbeseite_2")
 let suchfeld = document.getElementById("suchfeld")
 let werbung_die_nicht_von_adblockern_geblockt_werden_soll = document.getElementById("werbung_die_nicht_von_adblockern_geblockt_werden_soll")
 let footer = document.querySelector("footer")
+let li = document.getElementsByTagName("li")
+let suchmodus = false
 erste_seite.style["transition"] = "opacity 200ms"
 zweite_seite.style["transition"] = "opacity 200ms"
 let erste_seite_sichtbar = true
@@ -30,15 +32,15 @@ weiter_links.addEventListener("click", function() {
         erste_seite_sichtbar = true
     }
 })
-suchfeld.addEventListener("focus", function() {
-    let li = document.getElementsByTagName("li")
-    console.log(li)
-    werbung_die_nicht_von_adblockern_geblockt_werden_soll.style["transition"] = "opacity 200ms"
-    werbung_die_nicht_von_adblockern_geblockt_werden_soll.style["opacity"] = 0
-    footer.style["transition"] = "background-color 200ms"
-    footer.style["background-color"] = "white"
-    for (let i = 0; i < li.length; i++) {
-        li[i].style["transition"] = "color 100ms"
-        li[i].style["color"] = "black"
+suchfeld.addEventListener("change", function() {
+    if (!suchmodus) {
+        werbung_die_nicht_von_adblockern_geblockt_werden_soll.style["transition"] = "opacity 200ms"
+        werbung_die_nicht_von_adblockern_geblockt_werden_soll.style["opacity"] = 0
+        footer.style["transition"] = "background-color 200ms"
+        footer.style["background-color"] = "white"
+        for (let i = 0; i < li.length; i++) {
+            li[i].style["transition"] = "color 100ms"
+            li[i].style["color"] = "black"
+        }
     }
 })
