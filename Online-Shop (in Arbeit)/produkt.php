85368b96760bfae27c1146a23e0a8acc7bcf427f
LabraxAI <michel-bartels@web.de>
2017-11-01 11:57:38 +0100

    Update produkt.php

--- Online-Shop (in Arbeit)/produkt.php
+++ Online-Shop (in Arbeit)/produkt.php
@@ -2,21 +2,21 @@
 <html>
     <head>
         <title><?php
- -            $mysql = new mysqli("diff9.tk", "michel", "SB4me8w8t7KcjYIq", "michel_robin");
- -            if ($mysql->connect_error) {
- -                echo $mysql->connect_error;
- -            }
- -            $anfrage = $mysql->query("SELECT * FROM produkte WHERE ID = " . $_GET["id"]);
- -            $anfrage = $anfrage->fetch_assoc();
- -            $name $anfrage["Name"];
- -            echo $name;
- -            ?>
+            $mysql = new mysqli("diff9.tk", "michel", "SB4me8w8t7KcjYIq", "michel_robin");
+            if ($mysql->connect_error) {
+                echo $mysql->connect_error;
+            }
+            $anfrage = $mysql->query("SELECT * FROM produkte WHERE ID = " . $_GET["id"]);
+            $anfrage = $anfrage->fetch_assoc();
+            $name $anfrage["Name"];
+            echo $name;
+            ?>
         </title>
     </head>
     <body>
         <h1><?php
- -            echo $name;
- -            ?>
+            echo $name;
+            ?>
         </h1>
     </body>
 </html>
