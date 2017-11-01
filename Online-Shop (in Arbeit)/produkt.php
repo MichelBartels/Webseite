4339000ba858efe446b79aa2d3950f3365fa1d1d
LabraxAI <michel-bartels@web.de>
2017-11-01 12:39:39 +0100

    Update produkt.php

--- Online-Shop (in Arbeit)/produkt.php
+++ Online-Shop (in Arbeit)/produkt.php
@@ -21,7 +21,7 @@
                 unset($bilder[1]);
                 $Bilder = array();
                 foreach ($bilder as $bild) {
-                    $Bilder[] = "Bilder/Datenbank/" . $anfrage["Bild"] . "/" . $bild;
+                    $Bilder[] = "Bilder/Datenbank/" . $anfrage["Bild"] . $bild;
                 }
                 echo "let Bilder = JSON.parse('" . json_encode($Bilder) . "')";
             ?>
