55ed2fb72223e3a7443843561aaa87e9f78ae1c0
LabraxAI <michel-bartels@web.de>
2017-11-04 17:22:39 +0100

    Update suche_aktualisieren.php

--- Online-Shop (in Arbeit)/suche_aktualisieren.php
+++ Online-Shop (in Arbeit)/suche_aktualisieren.php
@@ -3,13 +3,18 @@
     if ($mysql->connect_error) {
         echo $mysql->connect_error;
     }
-    $anfrage = $mysql->query("SELECT Beschreibung FROM produkte");
-    $beschreibung = array();
+    $anfrage = $mysql->query("SELECT * FROM produkte");
+    $produkte = array();
     while ($zeile = $anfrage->fetch_assoc()) {
-        $beschreibung[] = $zeile;
+        $produkte[] = $zeile;
     }
     for ($i = 0; $i < count($beschreibung); $i++) {
-        $mysql->query("UPDATE produkte SET Suchinformationen = '" . json_encode(array_count_values(explode(" ", strtolower(str_replace("&szlig;", "ß", str_replace("&Uuml;", "Ü", str_replace("&Ouml;", "Ö", str_replace("&Auml;", "Ä", str_replace("&uuml;", "ü", str_replace("&ouml;", "ö", str_replace("&auml;", "ä", str_replace("“", "", str_replace("„", "", str_replace('"', "", (str_replace(",", "", str_replace(".", "", $beschreibung[$i]["Beschreibung"])))))))))))))))), JSON_UNESCAPED_UNICODE) . "' WHERE ID = " . ($i + 1) . ";");
+        $schluessel = array_keys($produkte);
+        $gesamt = "";
+        for ($i = 0; $i < count($schluessel); $i++) {
+            $gesamt .= " " . $schluessel[$i];
+        }
+        $mysql->query("UPDATE produkte SET Suchinformationen = '" . json_encode(array_count_values(explode(" ", strtolower(str_replace("&szlig;", "ß", str_replace("&Uuml;", "Ü", str_replace("&Ouml;", "Ö", str_replace("&Auml;", "Ä", str_replace("&uuml;", "ü", str_replace("&ouml;", "ö", str_replace("&auml;", "ä", str_replace("“", "", str_replace("„", "", str_replace('"', "", (str_replace(",", "", str_replace(".", "", $gesamt)))))))))))))))), JSON_UNESCAPED_UNICODE) . "' WHERE ID = " . ($i + 1) . ";");
     }
     $mysql->close();
 ?>
