46e01ce788c929257a673f57b94705ba63c50551
LabraxAI <michel-bartels@web.de>
2017-11-04 17:16:42 +0100

    Update suche.js

--- Online-Shop (in Arbeit)/Ajax/Suche/suche.js
+++ Online-Shop (in Arbeit)/Ajax/Suche/suche.js
@@ -13,7 +13,7 @@ function suchen(suchtext) {
     let suche_array = suchtext.split(" ")
     let passend = {};
     for (let i = 0; i < suche_array.length; i++) {
-        suche_array[i] = toLowerCase(suche_array[i])
+        suche_array[i] = suche_array[i].toLowerCase()
         if (suche_array[i].endsWith("m") || suche_array[i].endsWith("m2") || suche_array[i].endsWith("m²") || suche_array[i].endsWith("M") || suche_array[i].endsWith("M2") || suche_array[i].endsWith("M²")) {
             let groesse = parseInt(suche_array[i])
             let am_besten_passend = [Inf, false]
