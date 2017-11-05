46ebd445a4f56ece10165106322078fe8807a939
LabraxAI <michel-bartels@web.de>
2017-11-05 11:15:31 +0100

    Update suche.js

--- Online-Shop (in Arbeit)/Ajax/Suche/suche.js
+++ Online-Shop (in Arbeit)/Ajax/Suche/suche.js
@@ -40,8 +40,9 @@ function suchen(suchtext) {
     let passende_haeuser = Object.keys(passend)
     let anzahl_passender_haeuser = passende_haeuser.length
     for (let i = 0; i < anzahl_passender_haeuser; i++) {
-        sortiert.push(abfrage[Object.keys(abfrage)[Object.values(abfrage).indexOf(groesster_wert_in_object(passende_haeuser))]])
+        let schluessel = Object.keys(passende_haeuser)[Object.values(passende_haeuser).indexOf(groesster_wert_in_object(passende_haeuser))]
+        console.log(schluessel)
+        sortiert.push(abfrage[schluessel])
     }
-    console.log(sortiert)
     return sortiert
 }
