a6e904d01a6fcf812a787653fcab47dadcb13aa7
LabraxAI <michel-bartels@web.de>
2017-11-06 20:29:24 +0100

    Update test2.php

--- test2.php
+++ test2.php
@@ -10,7 +10,6 @@
     <a href="index.php"><div id="Link1"><p>Warenkorb</p></div></a>
     <a href="index.php"><div id="Link2"><p>Anmeldung</p></div></a>
     <a href="index.php"><div id="Link2"><p>Werbepartner</p></div></a>
-   <img id="Bild" src="Online-Shop (in Arbeit)/Bilder/Design/Logo Hochkant.png">
   </div>
   <div id="Ausen">
     Z<br><br>U<br><br>M<br><br><br>M<br><br>E<br><br>N<br><br>&Uuml;
