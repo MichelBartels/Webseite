0632df20e6b18e3997eb4aaca622f5eb50bad697
oekoklaus <32864685+oekoklaus@users.noreply.github.com>
2017-11-06 19:25:19 +0100

    Update test2.php

--- test2.php
+++ test2.php
@@ -7,10 +7,10 @@
   <div id="Innen">
     <div id="Ueberschrift"><b><u>B&B<br> Architekten</u></b></div>
     <a href="index.php"><div id="Link1"><p>Startseite</p></div></a>
-    <a href="index.php"><div id="Link1"><p>Wahrenkorb</p></div></a>
+    <a href="index.php"><div id="Link1"><p>Warenkorb</p></div></a>
     <a href="index.php"><div id="Link1"><p>Anmeldung</p></div></a>
-    <a href="index.php"><div id="Link1"><p>Werbepartner</p></div></a>
-    <div id="Link1"><p>Startseite</p></div>
+    <a href="index.php"><div id="Link2"><p>Werbepartner</p></div></a>
+    <div id="Link1"><p>Bild</p></div>
   </div>
   <div id="Ausen">
     Z<br><br>U<br><br>M<br><br><br>M<br><br>E<br><br>N<br><br>&Uuml;
