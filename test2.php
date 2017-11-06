4efc1e1708ff5acaada9f9f2610097e8e4a214ca
oekoklaus <32864685+oekoklaus@users.noreply.github.com>
2017-11-06 17:44:53 +0100

    Update test2.php

--- test2.php
+++ test2.php
@@ -3,8 +3,14 @@
 <link rel="stylesheet" href="test2.css">
 </head>
 <body>
-<div id="Seite">
-Hier ist Text
-</div>
+<div id="Menue">
+  <div id="Innen">
+    link<br>
+    link<br>
+  </div>
+  <div id="Ausen">
+    Z<br><br>U<br><br>M<br><br><br>M<br><br>E<br><br>N<br><br>&Uuml;
+  </div>
+  </div>
 </body>
 </html>
