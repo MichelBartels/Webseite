788c02a18e1c3ab6962a986123049dd03f324390
LabraxAI <michel-bartels@web.de>
2017-11-06 20:45:21 +0100

    Update test2.php

--- test2.php
+++ test2.php
@@ -6,12 +6,12 @@
 <div id="Menue">
   <div id="Innen">
     <div id="Ueberschrift"><u>B&B<br> Architekten</u></div>
-    <a href="index.php"><div id="Link1"><p>Startseite</p></div></a>
-    <a href="index.php"><div id="Link1"><p>Warenkorb</p></div></a>
-    <a href="index.php"><div id="Link2"><p>Anmeldung</p></div></a>
-    <a href="index.php"><div id="Link2"><p>Werbepartner</p></div></a>
+    <a href="index.php"><div class="link"><p>Startseite</p></div></a>
+    <a href="index.php"><div class="link"><p>Warenkorb</p></div></a>
+    <a href="index.php"><div class="link"><p>Anmeldung</p></div></a>
+    <a href="index.php"><div class="link"><p>Werbepartner</p></div></a>
   </div>
-  <div id="Ausen">
+  <div id="Aussen">
     Z<br><br>U<br><br>M<br><br><br>M<br><br>E<br><br>N<br><br>&Uuml;
   </div>
   </div>
