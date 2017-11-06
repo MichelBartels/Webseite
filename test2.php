de00a5fd24467ed91cc727d2da7d5fe77ddecf8f
oekoklaus <32864685+oekoklaus@users.noreply.github.com>
2017-11-06 18:46:58 +0100

    Update test2.php

--- test2.php
+++ test2.php
@@ -5,7 +5,7 @@
 <body>
 <div id="Menue">
   <div id="Innen">
-  <img src="Online-Shop (in Arbeit)/Bilder/Design/Logo Hochkant.png">
+    <div id="Link1">Startseite</div>
   </div>
   <div id="Ausen">
     Z<br><br>U<br><br>M<br><br><br>M<br><br>E<br><br>N<br><br>&Uuml;
