4b82d4e86a0e44e490c38c12b8adddcae2b2549d
LabraxAI <michel-bartels@web.de>
2017-11-07 09:57:25 +0100

    Create test2.php

---
+++ test2.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+<link rel="stylesheet" href="test2.css">
+</head>
+<body>
+<div id="Menue">
+  <div id="Innen">
+    <div id="Ueberschrift"><b><u>B&B<br> Architekten</u></b></div>
+    <a href="index.php"><div id="Link1"><p>Startseite</p></div></a>
+    <a href="index.php"><div id="Link1"><p>Warenkorb</p></div></a>
+    <a href="index.php"><div id="Link2"><p>Anmeldung</p></div></a>
+    <a href="index.php"><div id="Link2"><p>Werbepartner</p></div></a>
+    <div id="Link1"><img id="Bild" src="Online-Shop (in Arbeit)/Bilder/Design/Logo Hochkant.png"></div>
+  </div>
+  <div id="Ausen">
+    Z<br><br>U<br><br>M<br><br><br>M<br><br>E<br><br>N<br><br>&Uuml;
+  </div>
+  </div>
+</body>
+</html>
