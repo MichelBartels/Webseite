0366565ab2bbe29f886c22c96fed66050845a6d4
oekoklaus <32864685+oekoklaus@users.noreply.github.com>
2017-11-07 11:07:44 +0100

    Update produkt.php

--- Online-Shop/produkt.php
+++ Online-Shop/produkt.php
@@ -3,9 +3,7 @@
     <head>
         <link rel="stylesheet" href="CSS/produkt.css">
         <title>
-             <?php
-            include "menue.html";
-        ?>
+             
             <?php
             $mysql = new mysqli("diff9.tk", "michel", "SB4me8w8t7KcjYIq", "michel_robin");
             if ($mysql->connect_error) {
@@ -62,6 +60,9 @@
         </script>
     </head>
     <body>
+         <?php
+            include "menue.html";
+        ?>
         <h1><?php
             echo $name;
             ?>
