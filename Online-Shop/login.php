1bc05a5e26e931ad2834cb9029d3be85cd33abbc
LabraxAI <michel-bartels@web.de>
2017-11-12 17:03:44 +0100

    Update login.php

--- Online-Shop/login.php
+++ Online-Shop/login.php
@@ -4,4 +4,9 @@
         <title>Login</title>
         <link rel="stylesheet" href="CSS/login.css">
     </head>
+    <body>
+        <?php
+            include "menue.html";
+        ?>
+    </body>
 </html>
