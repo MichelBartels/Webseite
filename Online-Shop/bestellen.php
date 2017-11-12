bf22a79106d0d327a7df5f20f16b5b91f48e2cc7
LabraxAI <michel-bartels@web.de>
2017-11-12 18:34:08 +0100

    Update bestellen.php

--- Online-Shop/bestellen.php
+++ Online-Shop/bestellen.php
@@ -12,6 +12,9 @@
         <form>
             <input name="name" placeholder="Name" required><br>
             <input name="geburtsdatum" placeholder="Geburtsdatum (TT.MM.JJJJ)" pattern="\d\d.\d\d.\d\d\d\d" required><br>
-            <input name="adresse" placeholder="Geburtsdatum (TT.MM.JJJJ)" pattern="\d\d.\d\d.\d\d\d\d" required>
+            <input name="adresse" placeholder="Adresse" pattern="\d\d.\d\d.\d\d\d\d" required><br>
+            <input name="email" placeholder="Email-Adresse" type="email" required>
+            <button type="submit">Kostenpflichtig bestellen</button>
+        </form>
     </body>
 </html>
