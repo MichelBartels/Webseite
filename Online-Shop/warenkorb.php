07669bbdb8ffaeb063992f538a9710fd98cf3273
LabraxAI <michel-bartels@web.de>
2017-11-12 15:26:56 +0100

    Update warenkorb.php

--- Online-Shop/warenkorb.php
+++ Online-Shop/warenkorb.php
@@ -8,5 +8,19 @@
             include "menue.html";
         ?>
         <h1>Warenkorb</h1>
+        <?php
+        $mysql = new mysqli("18.194.170.23", "michel", "SB4me8w8t7KcjYIq", "michel_robin");
+        if ($mysql->connect_error) {
+            echo $mysql->connect_error;
+        }
+        session_start();
+        $warenkorb = array();
+        foreach ($_SESSION["Warenkorb"] as $id => $anzahl) {
+            $anfrage = $mysql->query("SELECT Name, Preis FROM produkte WHERE ID = " . $id);
+            $ergebnis = $anfrage->fetch_assoc();
+            $warenkorb[] = array("Name" => $ergebnis["Name"], "Preis" => $ergebnis["Preis"], "Anzahl" => $anzahl);
+        }
+        echo json_encode($warenkorb);
+        ?>
     </body>
 </html>
