086747ee8a6f54e5a7cf05d47671872ee068029f
LabraxAI <michel-bartels@web.de>
2017-11-12 16:07:42 +0100

    Update warenkorb_leeren.php

--- Online-Shop/warenkorb_leeren.php
+++ Online-Shop/warenkorb_leeren.php
@@ -1,6 +1,6 @@
 <?php
     session_start();
-    if (isset($_GET["id"]) {
+    if (isset($_GET["id"])) {
         unset($_SESSION["Warenkorb"][$_GET["id"]]);
     } else {
         unset($_SESSION["Warenkorb"]);
