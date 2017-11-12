9e545c84c23dab8d565bcdb5c4d6e1d423853a0f
LabraxAI <michel-bartels@web.de>
2017-11-12 15:44:06 +0100

    Update warenkorb.php

--- Online-Shop/warenkorb.php
+++ Online-Shop/warenkorb.php
@@ -17,20 +17,21 @@
                 </tr>
             </thead>
             <tbody>
-                <?php
-                    $mysql = new mysqli("18.194.170.23", "michel", "SB4me8w8t7KcjYIq", "michel_robin");
-                    if ($mysql->connect_error) {
-                        echo $mysql->connect_error;
-                    }
-                    session_start();
-                    $warenkorb = array();
-                    foreach ($_SESSION["Warenkorb"] as $id => $anzahl) {
-                        $anfrage = $mysql->query("SELECT Name, Preis FROM produkte WHERE ID = " . $id);
-                        $ergebnis = $anfrage->fetch_assoc();
-                        $warenkorb[] = array("Name" => $ergebnis["Name"], "Preis" => $ergebnis["Preis"], "Anzahl" => $anzahl);
-                    }
-                    echo json_encode($warenkorb);
-                ?>
+                <tr>
+                    <?php
+                        $mysql = new mysqli("18.194.170.23", "michel", "SB4me8w8t7KcjYIq", "michel_robin");
+                        if ($mysql->connect_error) {
+                            echo $mysql->connect_error;
+                        }
+                        session_start();
+                        $warenkorb = array();
+                        foreach ($_SESSION["Warenkorb"] as $id => $anzahl) {
+                            $anfrage = $mysql->query("SELECT Name, Preis FROM produkte WHERE ID = " . $id);
+                            $ergebnis = $anfrage->fetch_assoc();
+                            echo '<td class="name">' . $ergebnis["Name"] . '</td><td class="anzahl">' . $anzahl . '</td><td class="preis">' . $ergebnis["Preis"] . '</td>';
+                        }
+                    ?>
+                </tr>
             </tbody>
         </table>
     </body>
