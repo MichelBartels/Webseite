a3ed693b955ee5b2b0b9aab7e7eecdf98767612c
LabraxAI <michel-bartels@web.de>
2017-11-12 16:16:22 +0100

    Update warenkorb.php

--- Online-Shop/warenkorb.php
+++ Online-Shop/warenkorb.php
@@ -18,20 +18,18 @@
                 </tr>
             </thead>
             <tbody>
-                <tr>
-                    <?php
-                        $mysql = new mysqli("18.194.170.23", "michel", "SB4me8w8t7KcjYIq", "michel_robin");
-                        if ($mysql->connect_error) {
-                            echo $mysql->connect_error;
-                        }
-                        $warenkorb = array();
-                        foreach ($_SESSION["Warenkorb"] as $id => $anzahl) {
-                            $anfrage = $mysql->query("SELECT Name, Preis FROM produkte WHERE ID = " . $id);
-                            $ergebnis = $anfrage->fetch_assoc();
-                            echo '<td class="name">' . $ergebnis["Name"] . '</td><td class="anzahl">' . $anzahl . '</td><td class="preis">' . number_format($ergebnis["Preis"], 2, ",", " ") . ' €</td><td class="loeschen"><a href="warenkorb_leeren.php?url=index.php&id=' . $id . '">X</a></td>';
-                        }
-                    ?>
-                </tr>
+                <?php
+                    $mysql = new mysqli("18.194.170.23", "michel", "SB4me8w8t7KcjYIq", "michel_robin");
+                    if ($mysql->connect_error) {
+                        echo $mysql->connect_error;
+                    }
+                    $warenkorb = array();
+                    foreach ($_SESSION["Warenkorb"] as $id => $anzahl) {
+                        $anfrage = $mysql->query("SELECT Name, Preis FROM produkte WHERE ID = " . $id);
+                        $ergebnis = $anfrage->fetch_assoc();
+                        echo '<tr><td class="name">' . $ergebnis["Name"] . '</td><td class="anzahl">' . $anzahl . '</td><td class="preis">' . number_format($ergebnis["Preis"], 2, ",", " ") . ' €</td><td class="loeschen"><a href="warenkorb_leeren.php?url=index.php&id=' . $id . '">X</a></td></tr>';
+                    }
+                ?>
             </tbody>
         </table>
         <a href="warenkorb_leeren.php?url=index.php" id="warenkorb_leeren">Warenkorb komplett leeren</a>
