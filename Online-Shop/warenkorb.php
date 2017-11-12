e90394241e8e79b53a9ada86efeef6a8294a8f7f
LabraxAI <michel-bartels@web.de>
2017-11-12 16:15:24 +0100

    Update warenkorb.php

--- Online-Shop/warenkorb.php
+++ Online-Shop/warenkorb.php
@@ -28,7 +28,7 @@
                         foreach ($_SESSION["Warenkorb"] as $id => $anzahl) {
                             $anfrage = $mysql->query("SELECT Name, Preis FROM produkte WHERE ID = " . $id);
                             $ergebnis = $anfrage->fetch_assoc();
-                            echo '<td class="name">' . $ergebnis["Name"] . '</td><td class="anzahl">' . $anzahl . '</td><td class="preis">' . number_format($ergebnis["Preis"], 2, ",", " ") . ' €</td><td class="loeschen"><a href="warenkorb_leeren.php?url=index.php&id=' . $id . '"></a></td>';
+                            echo '<td class="name">' . $ergebnis["Name"] . '</td><td class="anzahl">' . $anzahl . '</td><td class="preis">' . number_format($ergebnis["Preis"], 2, ",", " ") . ' €</td><td class="loeschen"><a href="warenkorb_leeren.php?url=index.php&id=' . $id . '">X</a></td>';
                         }
                     ?>
                 </tr>
