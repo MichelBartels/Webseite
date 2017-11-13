b8dde4528306af44ac859ccba5990c1b3b0a1f90
LabraxAI <michel-bartels@web.de>
2017-11-13 20:00:56 +0100

    Update bestellt.php

--- Online-Shop/bestellt.php
+++ Online-Shop/bestellt.php
@@ -13,6 +13,16 @@
 <html>
     <head>
         <title>Bestellung erfolgreich</title>
+        <style>
+            h1 {
+                position: absolute;
+                top: 0;
+                left: 0;
+                width: 100%;
+                height: 100%;
+                text-align: center;
+            }
+        </style>
     </head>
     <body>
         <h1>Bestellung erfolgreich</h1>
