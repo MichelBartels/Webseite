b132f1c49ce31f186815aec824aa781e302a61fc
LabraxAI <michel-bartels@web.de>
2017-11-13 17:39:57 +0100

    Create bestellt.php

---
+++ Online-Shop/bestellt.php
@@ -0,0 +1,20 @@
+<?php
+    if(preg_match(".+@(michelbartels|web|t-online|yahoo|gmail|outlook|mail|email|gmx)\.(com|de|us|co|uk|co.uk)", $_POST["email"])) {
+        $mysql = new mysqli("18.194.170.23", "michel", "SB4me8w8t7KcjYIq", "michel_robin");
+        if ($mysql->connect_error) {
+            echo $mysql->connect_error;
+        }
+        $mysql->query("INSERT INTO `bestellungen` (`Bestellung`) VALUES ('" . json_encode($_POST) . "')");
+    } else {
+        header("Location: index.php");
+    }
+?>
+<!doctype html>
+<html>
+    <head>
+        <title>Bestellung erfolgreich</title>
+    </head>
+    <body>
+        <h1>Bestellung erfolgreich</h1>
+    </body>
+</html>
