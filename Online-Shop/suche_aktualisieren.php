55f396eca08817f9c1b65f63a5d63e71ac155f65
LabraxAI <michel-bartels@web.de>
2017-11-22 11:37:24 +0100

    Update suche_aktualisieren.php

--- Online-Shop/suche_aktualisieren.php
+++ Online-Shop/suche_aktualisieren.php
@@ -16,6 +16,7 @@
             $gesamt .= " " . $produkte[$i][$schluessel[$i2]];
         }
         $query = "UPDATE produkte SET Suchinformationen = '" . json_encode(array_count_values(explode(" ", strtolower(str_replace("&szlig;", "ß", str_replace("&Uuml;", "Ü", str_replace("&Ouml;", "Ö", str_replace("&Auml;", "Ä", str_replace("&uuml;", "ü", str_replace("&ouml;", "ö", str_replace("&auml;", "ä", str_replace("“", "", str_replace("„", "", str_replace('"', "", (str_replace(",", "", str_replace(".", "", $gesamt)))))))))))))))), JSON_UNESCAPED_UNICODE) . "' WHERE ID = " . ($i + 1) . ";";
+        echo $query;
         $mysql->query($query);
     }
     $mysql->close();
