ec270426d94ce4223d765bb112c3298122a6d7a7
oekoklaus <32864685+oekoklaus@users.noreply.github.com>
2017-12-12 10:36:49 +0100

    Update Produktliste.php

--- Online-Shop/Produktliste.php
+++ Online-Shop/Produktliste.php
@@ -1,9 +1,15 @@
 <html>
   <head>
      <link rel="stylesheet" href="CSS/Produktliste.css">
+    <titel>Produktliste</title>
   </head>
   <body>
+    <div id="Ueberschrift">
+      <u>Produktliste</u>
+      </div>
+    
   </body>
+  
 </html>
 
 
