fe691020927fc492b701d543abd07e32fe22b9ac
oekoklaus <32864685+oekoklaus@users.noreply.github.com>
2017-12-13 11:41:19 +0100

    Update Werbepartner.php

--- Online-Shop/Werbepartner.php
+++ Online-Shop/Werbepartner.php
@@ -4,7 +4,7 @@
         <title>Werbepartner</title>
         <link rel="stylesheet" href="CSS/Werbepartner.css">
     </head>
-    <body>
+    <body id="Body">
      <?php
             include "menue.html";
         ?>
@@ -12,18 +12,18 @@
         <br>
         <h1> Ikea </h3><br>
         <center><a href="http://www.ikea.com/de/de/"><img id="begrenzung" src="./Bilder/Design/ikea.jpg"></a></center><br>
-        <div>Finden sie tolle Inneneinrichtungen für ihr neues Eigenheim.<br>
+        <div id="DIV">Finden sie tolle Inneneinrichtungen für ihr neues Eigenheim.<br>
         Schauen sie auf der Internetseite vorbei oder besuchen sie sie persönlich in einer
         Ikeafiliale in ihrer nähe (Klick auf das Bild um auf der Webseite Vorbezuschauen)</div>
         <br>
         <h1> EdelErden </h3><br>
         <center><a href="../../Fabian&Nick Html/Onlineshop/"><img id="begrenzung" src="./Bilder/Design/edelerden2.jpg"></a></center><br>
-        <div>EdelErden ist unser Korparationspartner für eine Gemeinsamme Zukunft.<br>
+        <div id="DIV">EdelErden ist unser Korparationspartner für eine Gemeinsamme Zukunft.<br>
         Entdecken sie die erlesenzten Erden aus allen Ecken der Welt und Kaufen sie ein um sich selbst auch balt Erdprofi nenen zu können</div>
         <br>
         <h1> OBI </h3><br>
         <center><a href="https://www.obi.de/"><img id="begrenzung" src="./Bilder/Design/obi.jpeg"></a></center><br>
-        <div>Die Experten der OBI Gartenabteilung kümmern sich darum das das nach dem Bau ihres Hauses auch der Garten nicht zu kurz kommt
+        <div id="DIV">Die Experten der OBI Gartenabteilung kümmern sich darum das das nach dem Bau ihres Hauses auch der Garten nicht zu kurz kommt
         (unverbindliche preisempfelung)</div><br>
         <br>
     </body>
