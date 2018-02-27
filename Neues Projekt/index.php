214e92170e3a3d4de5f032ea37edd366a8d4ff66
oekoklaus <32864685+oekoklaus@users.noreply.github.com>
2018-02-27 10:32:49 +0100

    Update index.php

--- Neues Projekt/index.php
+++ Neues Projekt/index.php
@@ -9,7 +9,7 @@
       <u>Mindstorm NXT Sensorliste</u>
 </div>
    <div id="Texte">
-      Auf dieser Webseite Bescheftigen wir uns mit dem Mindstorm NXT auf diesem Teil der seite Listen wir alle Sensoren und ihre Funktionen  
+      Auf dieser Webseite Bescheftigen wir uns mit dem Mindstorm NXT<br> auf diesem Teil der seite Listen wir alle Sensoren und ihre Funktionen  
 </div>
     <div id="Liste">
        <div/>
