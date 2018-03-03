248f763325434b8159b54e35b8c07417f4324826
oekoklaus <32864685+oekoklaus@users.noreply.github.com>
2018-03-03 12:56:07 +0100

    Update index.php

--- Neues Projekt/index.php
+++ Neues Projekt/index.php
@@ -9,20 +9,20 @@
       <u>Mindstorm NXT Sensorliste</u>
 </div>
    <div id="Texte">
-      <img id="" src="unnamed.png">
+      <img id="bild" src="unnamed.png">
       Auf dieser Webseite Bescheftigen wir uns mit dem Mindstorm NXT<br> auf diesem Teil der seite Listen wir alle Sensoren und ihre Funktionen  
 </div>
     <div id="Liste">
        Der Tastsensor gibt dem Roboter einen Tastsinn, er detektiert 1,wenn er betatigt wird und 0, wenn er wieder freigegeben ist.<br><br>
-       <img id="" src="NXT-Touch-sensor.png">
+       <img id="bild" src="NXT-Touch-sensor.png">
        Der Tonsensor kann sowohl unangepasste Dezibelwerte [dB] wie auch angepasste Dezibelwerte [dBA] ermitteln Die Einheit Dezibel ist ein Mass für den Schalldruckpegel, wobei Werte zwischen 55 dB und 90 dB gemessen werden konnen. Diese Messwerte werdendurch den NXT in Prozenten ausgedruckt [%]. Je niedriger der Prozentwert,desto leiser ist die Tonquelle.
-       <img id="" src="NXT-Sound-Sensor.png">
+       <img id="bild" src="NXT-Sound-Sensor.png">
        <br><br>
        Der Lichtsensor ermöglicht dem Roboter, zwischen Hell und Dunkel zu unterscheiden. Er kann die Lichtintensitat in einem Raum (Helligkeit) oder einer farbigen Oberfläche messen
-       <img id="" src="NXT-Light-Sensor.png">
+       <img id="bild" src="NXT-Light-Sensor.png">
        <br><br>
        Der Ultraschallsensor ermöglicht dem Roboter Abstände zu Objekten zu berechnen, indem er die Zeit misst, die benötigt wird,bis eine von ihm ausgesendete Schallwelle an ein Objekt stösst und deren Echo wieder empfangen wird. Er misst Abstände in der Einheit Zentimeter oder Zoll und ist in der Lage Distanzen bis zu 255 cm mit einer Präzision von ± 3 cm zu messen.
-       <img id="" src="NXT-Ultrasonic-Sensor.png">
+       <img id="bild" src="NXT-Ultrasonic-Sensor.png">
        <br><br>
        <div/>
 </body>
