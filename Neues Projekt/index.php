23ab2e7bf65d8ec92956bbb817708f1f7b5b5e90
LabraxAI <michel-bartels@web.de>
2018-03-03 12:29:19 +0000

    Update index.php

--- Neues Projekt/index.php
+++ Neues Projekt/index.php
@@ -14,29 +14,21 @@
 </div>
     <div id="Liste">
        <div>
-       <p>
        Der Tastsensor gibt dem Roboter einen Tastsinn, er detektiert 1,wenn er betatigt wird und 0, wenn er wieder freigegeben ist.<br><br>
-       </p>
        <img id="bild" src="NXT-Touch-sensor.png">
        </div>
        <div>
-       <p>
        Der Tonsensor kann sowohl unangepasste Dezibelwerte [dB] wie auch angepasste Dezibelwerte [dBA] ermitteln Die Einheit Dezibel ist ein Mass für den Schalldruckpegel, wobei Werte zwischen 55 dB und 90 dB gemessen werden konnen. Diese Messwerte werdendurch den NXT in Prozenten ausgedruckt [%]. Je niedriger der Prozentwert,desto leiser ist die Tonquelle.
-       </p>
        <img id="bild" src="NXT-Sound-Sensor.png">
        <br><br>
        </div>
        <div>
-       <p>
        Der Lichtsensor ermöglicht dem Roboter, zwischen Hell und Dunkel zu unterscheiden. Er kann die Lichtintensitat in einem Raum (Helligkeit) oder einer farbigen Oberfläche messen
-       </p>
        <img id="bild" src="NXT-Light-Sensor.png">
        <br><br>
        </div>
        <div>
-       <p>
        Der Ultraschallsensor ermöglicht dem Roboter Abstände zu Objekten zu berechnen, indem er die Zeit misst, die benötigt wird,bis eine von ihm ausgesendete Schallwelle an ein Objekt stösst und deren Echo wieder empfangen wird. Er misst Abstände in der Einheit Zentimeter oder Zoll und ist in der Lage Distanzen bis zu 255 cm mit einer Präzision von ± 3 cm zu messen.
-       </p>
        <img id="bild" src="NXT-Ultrasonic-Sensor.png">
        <br><br>
        </div>
