c5dd33c7791abf24c72435b4281bb567e0058f22
oekoklaus <32864685+oekoklaus@users.noreply.github.com>
2018-03-06 09:52:30 +0100

    Update index.php

--- Neues Projekt/index.php
+++ Neues Projekt/index.php
@@ -12,23 +12,30 @@
       <img id="bild" src="unnamed.png">
       Auf dieser Webseite Bescheftigen wir uns mit dem Mindstorm NXT<br> auf diesem Teil der seite Listen wir alle Sensoren und ihre Funktionen  
 </div>
+  
     <div id="Liste">
-       <div>
+       
+       <div="Spalte">
        <img id="bild" src="NXT-Touch-sensor.png">
        Der Tastsensor gibt dem Roboter einen Tastsinn, er detektiert 1,wenn er betatigt wird und 0, wenn er wieder freigegeben ist.
        </div>
-       <div>
+          
+       <div="Spalte">   
        <img id="bild" src="NXT-Sound-Sensor.png">
        Der Tonsensor kann sowohl unangepasste Dezibelwerte [dB] wie auch angepasste Dezibelwerte [dBA] ermitteln Die Einheit Dezibel ist ein Mass für den Schalldruckpegel, wobei Werte zwischen 55 dB und 90 dB gemessen werden konnen. Diese Messwerte werdendurch den NXT in Prozenten ausgedruckt [%]. Je niedriger der Prozentwert,desto leiser ist die Tonquelle.
        </div>
-       <div>
+        
+       <div="Spalte">   
        <img id="bild" src="NXT-Light-Sensor.png">
        Der Lichtsensor ermöglicht dem Roboter, zwischen Hell und Dunkel zu unterscheiden. Er kann die Lichtintensitat in einem Raum (Helligkeit) oder einer farbigen Oberfläche messen
        </div>
-       <div>
+         
+       <div="Spalte">   
        <img id="bild" src="NXT-Ultrasonic-Sensor.png">
        Der Ultraschallsensor ermöglicht dem Roboter Abstände zu Objekten zu berechnen, indem er die Zeit misst, die benötigt wird,bis eine von ihm ausgesendete Schallwelle an ein Objekt stösst und deren Echo wieder empfangen wird. Er misst Abstände in der Einheit Zentimeter oder Zoll und ist in der Lage Distanzen bis zu 255 cm mit einer Präzision von ± 3 cm zu messen.
        </div>
+       
        </div>
+   
 </body>
 </html>
