98b5a2285619a7fadce2d527562cb6a44a9a3daf
oekoklaus <32864685+oekoklaus@users.noreply.github.com>
2018-04-24 10:33:55 +0200

    Update Touch-sensor.php

--- Neues Projekt/Touch-sensor.php
+++ Neues Projekt/Touch-sensor.php
@@ -19,7 +19,7 @@
    
         
        <div id="Spalte">   
-       <img id="bild" src="2">
+       <img id="bild" src="2.jpg">
       <br>
        Dieser Sensor trägt den Namen Berührungs-Sensor.<br>Man kann man die Art der Berührung einstellen (Druck, Freigabe, Stoß). Wird dieser Druck o.Ä. ausgeführt folgt der nächsten Befehl. 
        </div>
