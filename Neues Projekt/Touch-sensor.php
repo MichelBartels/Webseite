a5388a985455c273df34a2ae4a6c87938e058eeb
oekoklaus <32864685+oekoklaus@users.noreply.github.com>
2018-04-24 10:19:08 +0200

    Update Touch-sensor.php

--- Neues Projekt/Touch-sensor.php
+++ Neues Projekt/Touch-sensor.php
@@ -1,1 +1,82 @@
-
+au<!doctype html>
+<html>
+<head>
+   <link rel="stylesheet" href="index.css">
+  <title>Mindstorm projekt</title>
+</head>
+<body>
+   <div id="Ueeberschrift">
+      <u>Mindstorm NXT Sensorliste</u>
+</div>
+   <div id="Texte">
+      <img id="bild" src="unnamed.png">
+      Auf dieser Webseite beschäftigen wir uns mit dem Mindstorm NXT<br> in diesem Teil der Seite Listen wir alle Sensoren und ihre Funktionen auf  
+</div>
+  
+    <div id="Liste">
+       
+       <div id="Spalte">
+       <img id="bild" src="NXT-Touch-sensor.png">
+       Durch den Tastsensor, erhält er Roboter einen Tastsinn, er detektiert 1, wenn er betätigt wird und 0, wenn er wieder freigegeben ist.
+         
+       </div>
+          
+       <div id="Spalte">   
+       <img id="bild" src="NXT-Sound-Sensor.png">
+       Der Tonsensor kann sowohl unangepasste Dezibelwerte [dB] wie auch angepasste Dezibelwerte [dBA] ermitteln Die Einheit Dezibel ist ein Mass für den Schalldruckpegel, wobei Werte zwischen 55 dB und 90 dB gemessen werden konnen. Diese Messwerte werden durch den NXT in Prozenten ausgedruckt [%]. Je niedriger der Prozentwert, desto leiser ist die Tonquelle.
+      
+       </div>
+        
+       <div id="Spalte">   
+       <img id="bild" src="NXT-Light-Sensor.png">
+       Der Lichtsensor ermöglicht dem Roboter, zwischen Hell und Dunkel zu unterscheiden. Er kann die Lichtintensität in einem Raum (Helligkeit) oder einer farbigen Oberfläche messen.
+       
+       </div>
+         
+       <div id="Spalte">   
+       <img id="bild" src="NXT-Ultrasonic-Sensor.png">
+       Der Ultraschallsensor ermöglicht dem Roboter Abstände zu Objekten zu berechnen, indem er die Zeit misst, die benötigt wird, bis eine von ihm ausgesendete Schallwelle an ein Objekt stösst und deren Echo wieder empfangen wird. Er misst Abstände in der Einheit Zentimeter oder Zoll und ist in der Lage Distanzen bis zu 255 cm mit einer Präzision von ± 3 cm zu messen.
+   
+       </div>
+       
+       </div>
+   <div id="Liste">
+       
+       <div id="Spalte">
+          <img id="bild" src="">
+         Hier sind einige Bilder um zu veranschaulichen, wie die verschiedenen Sensoren programmiert werden: 
+       </div>
+          
+       <div id="Spalte">   
+       <img id="bild" src="1">
+      Dieser Sensor trägt den Namen Zeit-Sensor.<br>Man kann eine Sekundenanzahl angeben. <br>Sobald diese Zeit abgewartet ist, führt der NXT den nächsten Befehl durch.
+       </div>
+        
+       <div id="Spalte">   
+       <img id="bild" src="2">
+       Dieser Sensor trägt den Namen Berührungs-Sensor.<br>Man kann man die Art der Berührung einstellen (Druck, Freigabe, Stoß). Wird dieser Druck o.ä. ausgeführt folgt der nächsten Befehl. 
+       </div>
+         
+       <div id="Spalte">   
+       <img id="bild" src="3">
+       Dieser Sensor trägt den Namen Licht-Sensor.<br>Man kann einstellen, wie groß die Lichtdifferenz sein soll, damit der nächste Befehl durchgeführt wird. <br>Und ob eine eigene Lichtquelle an sein soll, damit bei schwachem/keinem Licht immer noch alles funktioniert.   
+       </div>
+      
+       <div id="Spalte">   
+       <img id="bild" src="4">
+       Dieser Sensor trägt den Namen Geräusche-Sensor. <br>Man kann einstellen wie laut oder leise ein Geräusch (bsw. Klatschen oder ein Schrei) sein soll, damit der NXT den nächsten Befehl durchführt.  
+       </div>
+      
+     
+       <div id="Spalte">   
+       <img id="bild" src="5"> 
+       Dieser Sensor trägt den Namen Ultraschall-Sensor. <br>Man kann einstellen wie groß die Distanz zu einem anderen Objekt sein soll, damit der NXT den nächsten Befehl durchführt.
+          
+       </div>
+     
+      
+</div>
+   
+</body>
+</html>
+ 
