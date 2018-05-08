b76ed55f08e4ff973e80b21debce4f599a995b77
oekoklaus <32864685+oekoklaus@users.noreply.github.com>
2018-05-08 10:34:25 +0200

    Create Light-Sensor.php

---
+++ Neues Projekt/Light-Sensor.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html>
+<head>
+   <link rel="stylesheet" href="touch-sensor.css">
+  <title>Mindstorm projekt</title>
+</head>
+<body>
+   <div id="Ueeberschrift">
+      <u>Mindstorm NXT Sensorliste</u>
+   </div>
+  
+   
+   <div id="Liste">
+       
+   
+        
+       <div id="Spalte">   
+       <img id="bild" src="2.jpg">
+    
+       Dieser Sensor trägt den Namen Berührungs-Sensor.<br>Man kann die Art der Berührung einstellen (Druck, Freigabe, Stoß). Wird dieser Druck o.Ä. ausgeführt folgt der nächsten Befehl. 
+       </div>
+         
+    
+     
+      
+</div>
+   
+</body>
+</html>
+ 
