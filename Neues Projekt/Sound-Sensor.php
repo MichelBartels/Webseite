a5083cb09b3ab2a5516fa7e191143e2b9e7a4236
oekoklaus <32864685+oekoklaus@users.noreply.github.com>
2018-05-08 09:57:18 +0200

    Update Sound-Sensor.php

--- Neues Projekt/Sound-Sensor.php
+++ Neues Projekt/Sound-Sensor.php
@@ -1,1 +1,36 @@
 
+<!doctype html>
+<html>
+<head>
+   <link rel="stylesheet" href="Sound-Sensor.css">
+  <title>Mindstorm projekt</title>
+</head>
+<body>
+   <div id="Ueeberschrift">
+      <u>Mindstorm NXT Sensorliste</u>
+</div>
+   <div id="Texte">
+      <img id="bild" src="unnamed.png">
+      <br>
+      Auf dieser Webseite beschäftigen wir uns mit Garnichts<br> in diesem Teil der Seite Listen wir alle Sensoren und ihre Funktionen auf  
+</div>
+  
+   
+   <div id="Liste">
+       
+   
+        
+       <div id="Spalte">   
+       <img id="bild" src="2.jpg">
+      <br>
+       Dieser Sensor trägt den Namen Berührungs-Sensor.<br>Man kann man die Art der Berührung einstellen (Druck, Freigabe, Stoß). Wird dieser Druck o.Ä. ausgeführt folgt der nächsten Befehl. 
+       </div>
+         
+    
+     
+      
+</div>
+   
+</body>
+</html>
+ 
