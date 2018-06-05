de9d07bc5784ba56afe09e400dfad19d2cfb1505
oekoklaus <32864685+oekoklaus@users.noreply.github.com>
2018-06-05 09:55:51 +0200

    Update Ultraschall-Sensor.php

--- Neues Projekt/Ultraschall-Sensor.php
+++ Neues Projekt/Ultraschall-Sensor.php
@@ -17,7 +17,7 @@
        <div id="Spalte">   
        <img id="bild" src="2.jpg">
     
-      Dieser Sensor trägt den Namen Licht-Sensor. Man kann einstellen, wie groß die Lichtdifferenz sein soll, damit der nächste Befehl durchgeführt wird und ob eine eigene Lichtquelle an sein soll, damit bei schwachem/keinem Licht immer noch Alles funktioniert.   
+      Dieser Sensor trägt den Namen Ultraschall-Sensor. <br>Man kann einstellen wie groß die Distanz zu einem anderen Objekt sein soll, damit der NXT den nächsten Befehl durchführt.
        </div>
          
     
