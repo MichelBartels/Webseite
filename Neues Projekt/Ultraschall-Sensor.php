d42e1ee42b5bb0dbbf20791ed22b4913cb0d5067
oekoklaus <32864685+oekoklaus@users.noreply.github.com>
2018-06-05 09:55:14 +0200

    Create Ultraschall-Sensor.php

---
+++ Neues Projekt/Ultraschall-Sensor.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html>
+<head>
+   <link rel="stylesheet" href="touch-sensor.css">
+  <title>Mindstorm projekt</title>
+</head>
+<body>
+   <div id="Ueeberschrift">
+      <u>Mindstorm NXT Sensorliste</u>
+   </div>
+  
+   
+   <div id="Liste">
+       
+   
+        
+       <div id="Spalte">   
+       <img id="bild" src="2.jpg">
+    
+      Dieser Sensor trägt den Namen Licht-Sensor. Man kann einstellen, wie groß die Lichtdifferenz sein soll, damit der nächste Befehl durchgeführt wird und ob eine eigene Lichtquelle an sein soll, damit bei schwachem/keinem Licht immer noch Alles funktioniert.   
+       </div>
+         
+    
+     
+      
+</div>
+   
+</body>
+</html>
