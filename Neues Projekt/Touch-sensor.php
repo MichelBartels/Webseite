5824e02c2e5159620616e626f70f2c4a4a8c263b
LabraxAI <michel-bartels@web.de>
2018-06-26 10:43:28 +0200

    Update Touch-sensor.php

--- Neues Projekt/Touch-sensor.php
+++ Neues Projekt/Touch-sensor.php
@@ -1,30 +1,112 @@
-<!doctype html>
+ <html>
+<head>
+<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">     
+<link rel="stylesheet" href="layout.css">
+</head>
+
+<body>
 <html>
 <head>
-   <link rel="stylesheet" href="touch-sensor.css">
-  <title>Mindstorm projekt</title>
+<link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">     
+<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet"> 
+<link href="https://fonts.googleapis.com/css?family=PT+Sans" rel="stylesheet"> 
+<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet"> 
+<link href="https://fonts.googleapis.com/css?family=Work+Sans" rel="stylesheet"> 
+<title>Mindstorms-Sensoren</title>
+<link rel="shortcut icon" type="image/x-icon" href="Bilder/Bild.jpg">
+<meta charset="utf-8">
 </head>
+
 <body>
-   <div id="Ueeberschrift">
-      <u>Mindstorm NXT Sensorliste</u>
-   </div>
-  
-   
-   <div id="Liste">
-       
-   
-        
-       <div id="Spalte">   
-       <img id="bild" src="5.jpg">
-    
-       Dieser Sensor trägt den Namen Berührungssensor.<br>Man kann die Art der Berührung einstellen (Druck, Freigabe, Stoß). Wird dieser Druck o.Ä. ausgeführt folgt der nächsten Befehl. 
-       </div>
-         
-    
-     
-      
+<header> 
+<div id="Überschrift">
+Mindstorms-Sensoren
+
 </div>
-   
+</header>
+
+<header id="Sticky">
+<header id="Navigation">
+
+
+
+<div class="dropdown">
+<button class="dropbtn">
+Home
+</button>
+
+<div class="dropdown-content">
+<a href="index.html">Mindstorms-Home </a>
+</div>
+</div>
+
+
+
+<a href="sensoren.html">
+<div class="dropdown">
+<button class="dropbtn">
+Sensoren
+</button>
+<div class="dropdown-content">
+<div id="klein">
+<a href="sensoren.html">
+Mehr Details
+</a>
+</div> 
+</div>
+</div>
+</a>
+
+
+
+<a href="mailto:login@michelbartels.com">
+<div class="dropdown">
+<button class="dropbtn">
+Kontakt
+</button>
+<div class="dropdown-content">
+<div id="klein">
+<a href="mailto:login@michelbartels.com">
+Mail an die Ersteller senden.
+(Fragen,
+Anregungen,
+Wünsche)
+</a>
+
+</div>
+</div>
+</div>
+</a>
+
+
+</div>
+</div>
+
+
+</header>
+</header>
+
+
+<div style='text-align: right;position: fixed;z-index:9999999;bottom: 0; width: 100%;cursor: pointer;line-height: 0;display:block !important;'><a title="000webhost logo" rel="nofollow" target="_blank" href="https://www.000webhost.com/?utm_source=000webhostapp&amp;utm_campaign=000_logo&amp;utm_campaign=ss-footer_logo&amp;utm_medium=000_logo&amp;utm_content=website"><img src="https://cdn.rawgit.com/000webhost/logo/e9bd13f7/footer-powered-by-000webhost-white2.png" alt="000webhost logo"></a></div></body>
+</html>
+
+
+<div id="Artikel">
+<img src="5.jpg" alt="Bild kann nicht angezeigt werden" id="Gold">
+<h1>
+Dieser Sensor trägt den Namen Berührungssensor.<br>Man kann die Art der Berührung einstellen (Druck, Freigabe, Stoß). Wird dieser Druck o.Ä. ausgeführt folgt der nächsten Befehl.
+</h1>
+</div>
+
+<div id="Divbox">
+</div>
+
+<footer>
+<center>
+Made by Fabian, Romy, Robin and Michel
+</center>
+</footer>
+
 </body>
 </html>
- 
+
